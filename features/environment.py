2949a98ab5fe5a5188249bbba1faebe9bc6416ea
Noah Jones <noahsj@cox.net>
2024-01-27 15:01:06 -0800

    Browserstack App Automate Attempt 1

--- features/environment.py
+++ features/environment.py
@@ -1,6 +1,6 @@
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
-
+from selenium.webdriver.chrome.options import Options
 from app.application import Application
 
 
@@ -8,24 +8,45 @@ def mobile_driver_init(context, scenario_name):
     """
     :param context: Behave context
     """
-    desired_capabilities = {
-        "platformName": "Android",
-        "automationName": 'uiautomator2',
-        "platformVersion": "10",
-        "deviceName": "Android Emulator",
-        "appActivity": "org.wikipedia.main.MainActivity",
-        "appPackage": "org.wikipedia",
-        # Put your path below:
-        "app": "C:/Users/noahsj/AppiumAutomation/mobile_app/wikipedia.apk"
+    # desired_capabilities = {
+    #     "platformName": "Android",
+    #     "automationName": 'uiautomator2',
+    #     "platformVersion": "10",
+    #     "deviceName": "Android Emulator",
+    #     "appActivity": "org.wikipedia.main.MainActivity",
+    #     "appPackage": "org.wikipedia",
+    #     # Put your path below:
+    #     "app": "C:/Users/noahsj/AppiumAutomation/mobile_app/wikipedia.apk"
+    # }
+    #
+    # appium_server_url = 'http://localhost:4723'
+    # capabilities_options = UiAutomator2Options().load_capabilities(desired_capabilities)
+    #
+    # context.driver = webdriver.Remote(appium_server_url, options=capabilities_options)
+    # context.driver.implicitly_wait(5)
+    #
+    # context.app = Application(context.driver)
+
+
+###BROWSERSTACK###
+    bs_user = 'noahsj_p3KDFs',
+    bs_key = 'BAiKTuqj9kkQtzaDyRBE',
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    app = 'bs://6ef5bd11a08f26d8ca4a095c5b7dc85d5e8b1c64',
+    options = Options()
+    browserstack_caps = {
+        # 'browserstack.networkLogs': 'true',
+        'device': 'Samsung Galaxy S21',
+        'os_version': '11.0',
+        'automationName': 'UiAutomator2',
+        'project': 'Appium Wiki Test',
+        'build': 'Python Android',
+        'name': scenario_name
     }
-
-    appium_server_url = 'http://localhost:4723'
-    capabilities_options = UiAutomator2Options().load_capabilities(desired_capabilities)
-
-    context.driver = webdriver.Remote(appium_server_url, options=capabilities_options)
+    options.set_capability('bstack:options', browserstack_caps)
+    context.browserstack_driver = webdriver.Remote('http://hub-cloud.browserstack.com/wd/hub', options=options)
     context.driver.implicitly_wait(5)
-
-    context.app = Application(context.driver)
+    context.app = Application(context.browserstack_driver)
 
 
 def before_scenario(context, scenario):
@@ -39,9 +60,11 @@ def before_step(context, step):
 
 def after_step(context, step):
     if step.status == 'failed':
-        context.driver.save_screenshot(f'{step}.png')
+        # context.driver.save_screenshot(f'{step}.png')
         print('\nStep failed: ', step)
 
 
 def after_scenario(context, feature):
-    context.driver.quit()
+    # context.driver.quit()
+    context.browserstack_driver.quit()
+
